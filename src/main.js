032978a987aba93b85d74772ced6736fb0e6ff7d
Gblanquez <hellogabrielblanquez@gmail.com>
2025-06-20 23:06:18 +0800

    load

--- src/main.js
+++ src/main.js
@@ -1,63 +1,7 @@
 import './styles/style.css'
-import { gsap } from 'gsap'
-import SplitText from 'gsap/SplitText'
-import ScrollTrigger from 'gsap/ScrollTrigger'
-import loadImages from './load/load.js'
-import { initSmoothScroll } from './smoothScroll/smoothScroll'
-import subNavButtons from './animations/buttons/subNavButtons'
-import clickMainButton from './animations/buttons/clickMainButton'
-import menuClick from './animations/menu/menuClick'
-import initIntroAnimation from './animations/introSection/introAnimation'
-import initCertificationAnimation from './animations/certificationSection/certificationSection'
-import initStoryDesktop from './animations/storySection/storySection'
-import initCollectionAnimation from './animations/collectionSection/collectionSection'
-import initCampLinkAnimation from './animations/campLink/campLink'
-import initItineraryAnimation from './animations/itinerarySection/itinerarySection'
-import footerLinks from './animations/footerLinks/footerLinks'
-import initNavLinkAnimation from './animations/navLinks/navLinks'
-import navSmallLinks from './animations/navLinks/navSmallLinks'
-import menuHover from './animations/menu/menuHover'
-import conservationPage from './animations/conservationPage/conservationPage'
-import relaisPage from './animations/relaisPage/relaisPage'
-import pressPage from './animations/PressPage/pressPage'
-import collectionPage from './animations/collectionPage/collectionPage'
-import contactPage from './animations/contactPage/contactPage'
-import storyPage from './animations/storyPage/storyPage'
 import './transitions' 
 
 
 
 
-// Initialize all animations
-// navSmallLinks()
-// initNavLinkAnimation()
-// initIntroAnimation()
-// initCampLinkAnimation()
-// initItineraryAnimation()
-// footerLinks()
-// initCollectionAnimation()
-// initCertificationAnimation()
-// initStoryDesktop()
-// initSmoothScroll()
-// menuHover()
-// menuClick()
-// clickMainButton()
-// subNavButtons()
 
-//Conservation Page
-// conservationPage()
-
-//Relais Page
-// relaisPage()
-
-//Press Page
-// pressPage()
-
-//Collection Page
-// collectionPage()
-
-//Contact Page
-// contactPage()
-
-//Story Page
-// storyPage()
