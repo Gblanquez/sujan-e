dcde7e562ad56523f3e95f929e7d60b6f5e61565
Gblanquez <hellogabrielblanquez@gmail.com>
2025-07-07 17:03:19 +0800

    no logo marqueee

--- src/animations/homePage/homePage.js
+++ src/animations/homePage/homePage.js
@@ -10,7 +10,7 @@ import logoMarquee from './logoMarquee'
 const homePage = () => {
     videoClick()
     initCampLinkAnimation()
-    logoMarquee()
+    // logoMarquee()
     initIntroAnimation()
     initItineraryAnimation()
     initCollectionAnimation()
