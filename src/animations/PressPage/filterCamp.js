d1008a0482e79e256612c4f1e209e964c931a676
Gblanquez <hellogabrielblanquez@gmail.com>
2025-08-14 19:12:44 +0800

    Press Page Update

---
+++ src/animations/PressPage/filterCamp.js
@@ -0,0 +1,61 @@
+import { gsap } from 'gsap';
+import { Draggable } from 'gsap/Draggable';
+import { InertiaPlugin } from 'gsap/InertiaPlugin';
+
+gsap.registerPlugin(Draggable, InertiaPlugin);
+
+const initFilterCampCarousel = () => {
+  const wrapper = document.querySelector('.press_camp_filter');
+  const list = document.querySelector('.press_camp_filter_list');
+
+  if (!wrapper || !list) return; // safety check
+
+  let draggableInstance;
+
+  function enableDraggable() {
+    const getMaxScroll = () => list.scrollWidth - wrapper.offsetWidth;
+    let maxScroll = getMaxScroll();
+
+    draggableInstance = Draggable.create(list, {
+      type: 'x',
+      inertia: true,
+      edgeResistance: 0.95,
+      bounds: {
+        minX: -maxScroll,
+        maxX: 0
+      },
+      allowContextMenu: true,
+      overshootTolerance: 0.15,
+      inertiaResistance: 20
+    })[0];
+
+    window.addEventListener('resize', () => {
+      maxScroll = getMaxScroll();
+      draggableInstance?.applyBounds({
+        minX: -maxScroll,
+        maxX: 0
+      });
+    });
+  }
+
+  function disableDraggable() {
+    if (draggableInstance) {
+      draggableInstance.kill();
+      draggableInstance = null;
+    }
+  }
+
+  function checkViewport() {
+    if (window.innerWidth <= 1100) {
+      if (!draggableInstance) enableDraggable();
+    } else {
+      disableDraggable();
+    }
+  }
+
+  // Initial check + listen for resize
+  checkViewport();
+  window.addEventListener('resize', checkViewport);
+};
+
+export default initFilterCampCarousel;
--- src/animations/PressPage/pressPage.js
+++ src/animations/PressPage/pressPage.js
@@ -3,6 +3,8 @@ import pressContent from './pressContent/pressContent'
 import pressCampLink from './PressLinkHover/pressCampLink'
 import pressLink from './PressLinkHover/pressLink'
 import { default as featuredButton } from '../buttons/featuredButton'
+import initFilterCampCarousel from './filterCamp'
+import initFilterYearCarousel from './yearCamp'
 
 const pressPage = () => {
     // pressH()
@@ -10,6 +12,8 @@ const pressPage = () => {
     pressCampLink()
     pressLink()
     featuredButton()
+    initFilterCampCarousel()
+    initFilterYearCarousel()
 }
 
 export default pressPage
